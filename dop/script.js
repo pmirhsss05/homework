244c4b49fb6b39bf0c7b5199abd83587b1ad6008
pmirhsss05 <150133913+pmirhsss05@users.noreply.github.com>
2023-11-08 01:40:40 +0300

    Create script.js

---
+++ dop/script.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Решение алгоритмической задачи</title>
+    <script src="script.js"></script>
+</head>
+<body>
+
+    Дано вещественное число. Определить, какое это число: положительное, отрицательное, ноль.
+    <input type="text" value="Дано вещественное число. Определить, какое это число: положительное, отрицательное, ноль.">
+    <p>Введите число:</p>
+    <input type="number" id="numberInput">
+    <button onclick="checkNumber()">Проверить</button>
+    <p id="result"></p>
+
+</body>
+</html>
