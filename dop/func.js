730c6fbe3d83582ddf571b03e526618094bb4956
pmirhsss05 <150133913+pmirhsss05@users.noreply.github.com>
2023-11-08 13:18:26 +0300

    Create func.js

---
+++ dop/func.js
@@ -0,0 +1,8 @@
+const changeText = () => {
+  document.getElementsByClassName('block1')[0].textContent = "UFMS RUSSIA MOSCOW";
+  document.getElementsByClassName('block4')[0].textContent = "DENISOVA";
+  document.getElementsByClassName('block5')[0].textContent = "KSENIA";
+  document.getElementsByClassName('block6')[0].textContent = "DENISOVNA";
+  document.getElementsByClassName('block7')[0].textContent = "FEMALE";
+  document.getElementsByClassName('block9')[0].textContent = "MOSCOW";
+}
